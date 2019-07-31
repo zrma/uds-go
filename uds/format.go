f1e0ec88f2442987e190cf72658b504a188e80b5
zrma <bulbitain@gmail.com>
2019-07-31 21:43:49 +0900

    Do refactoring: divide functions to calcSize and format.

--- uds/format.go
+++ uds/format.go
@@ -5,24 +5,35 @@ import (
 	"fmt"
 )
 
-func format(numOfBytes int64) (string, error) {
+var units = []string{"bytes", "KB", "MB", "GB", "TB"}
+
+type sizeIndex int
+
+const invalid sizeIndex = -1
+
+func calcSize(numOfBytes int64) (size float64, idx sizeIndex) {
 	if numOfBytes <= 0 {
-		return "", errors.New("size should be positive")
+		idx = invalid
+		return
 	}
 
 	step := 1024.
-
-	size := float64(numOfBytes)
-	units := []string{"bytes", "KB", "MB", "GB", "TB"}
-
-	var unit string
-	for i, u := range units {
-		unit = u
+	size = float64(numOfBytes)
+	for i := range units {
+		idx = sizeIndex(i)
 		if i == len(units)-1 || size/step < 1 {
 			break
 		}
 		size /= step
 	}
+	return
+}
 
-	return fmt.Sprintf("%.1f %s", size, unit), nil
+func format(numOfBytes int64) (string, error) {
+	size, idx := calcSize(numOfBytes)
+	if idx == invalid {
+		return "", errors.New("size should be positive")
+	}
+
+	return fmt.Sprintf("%.1f %s", size, units[idx]), nil
 }
