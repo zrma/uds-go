b3a2dc5797a16f26a15df921413f1c85a1015516
zrma <bulbitain@gmail.com>
2020-02-08 19:43:49 +0900

    Do refactoring: use pkg instead of legacy code in cmd/quickstart

--- cmd/quickstart/main.go
+++ cmd/quickstart/main.go
@@ -1,93 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
-	"path"
-	"path/filepath"
-	"runtime"
 
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
-	"google.golang.org/api/option"
+
+	"github.com/zrma/uds-go/pkg/api"
 )
 
-// Retrieve a token, saves the token, then returns the generated client.
-func getToken(config *oauth2.Config) *oauth2.Token {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := filepath.Join(GetCurrentDir(), "token.json")
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return tok
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
-	}
-	return tok
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
 func main() {
-	credentials := filepath.Join(GetCurrentDir(), "credentials.json")
-	b, err := ioutil.ReadFile(credentials)
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
-
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, drive.DriveScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-
-	token := getToken(config)
-	ctx := context.Background()
-	driveService, err := drive.NewService(ctx, option.WithTokenSource(config.TokenSource(ctx, token)))
+	driveService, err := api.NewService()
 	if err != nil {
 		log.Fatalf("Unable to retrieve NewService: %v", err)
 	}
@@ -120,16 +43,4 @@ func main() {
 			fmt.Printf("%s (%s)\n", i.Name, i.Id)
 		}
 	}
-}
-
-// GetCurrentFile works like __FILE__
-func GetCurrentFile() string {
-	_, filename, _, _ := runtime.Caller(1)
-	return filename
-}
-
-// GetCurrentDir works like __DIR__
-func GetCurrentDir() string {
-	_, filename, _, _ := runtime.Caller(1)
-	return path.Dir(filename)
 }
