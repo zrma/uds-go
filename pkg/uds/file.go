4b7cbe41cee7a39885fdfde5cb31c05e0aa91fe3
zrma <bulbitain@gmail.com>
2020-02-23 11:20:58 +0900

    Add initializational method of File and Chunk structs

--- pkg/uds/file.go
+++ pkg/uds/file.go
@@ -3,13 +3,40 @@ package uds
 // File struct is file wrapper
 type File struct {
 	Name        string
-	MD5         string
+	Mime        string
 	Size        string
 	EncodedSize string
 	SizeNumeric string
 	Parents     []string
+
+	ID     string
+	MD5    string
+	Shared bool
+}
+
+// Init method initialize File struct's parents not to be nil
+func (f *File) Init() {
+	if f.Parents == nil {
+		f.Parents = []string{"root"}
+	}
 }
 
 // Chunk struct is split file chunk
 type Chunk struct {
+	Path    string
+	Part    int64
+	MaxSize int64
+	Media   *File
+	Parent  string
+
+	RangeEnd int64
+}
+
+// Init method initialize Chunk struct's range end boundary
+func (c *Chunk) Init() {
+	const chunkReadLengthBytes int64 = 750000
+	c.RangeEnd = (c.Part + 1) * chunkReadLengthBytes
+	if c.RangeEnd > c.MaxSize {
+		c.RangeEnd = c.MaxSize
+	}
 }
